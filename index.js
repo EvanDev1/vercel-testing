48f677b6d8c28b45f7333ec1ea878eb42053ca25
Evan Larsen <evanlarsen2008@gmail.com>
2023-01-14 18:37:30 -0700

    Second commit

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use("/home", home);
+
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
